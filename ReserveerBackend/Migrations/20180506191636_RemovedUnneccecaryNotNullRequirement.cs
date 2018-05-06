d9df308113219c8c0d1771f2ec43cc2b67312440
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2018-05-06 21:17:19 +0200

    Added a migration and updated database

---
+++ ReserveerBackend/Migrations/20180506191636_RemovedUnneccecaryNotNullRequirement.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ReserveerBackend.Migrations
+{
+    public partial class RemovedUnneccecaryNotNullRequirement : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
