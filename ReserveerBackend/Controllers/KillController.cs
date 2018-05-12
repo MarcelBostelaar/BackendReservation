d935ae0f0e9ec01f529406c60cdd816398d2fad9
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2018-05-12 16:33:22 +0200

    Added kill method and simple quick fix for swagger error

---
+++ ReserveerBackend/Controllers/KillController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReserveerBackend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Kill")]
+    public class KillController : Controller
+    {
+        [HttpGet]
+        public void Kill()
+        {
+            Environment.Exit(0);
+        }
+    }
+}
